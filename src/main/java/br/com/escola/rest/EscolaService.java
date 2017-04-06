9039df201286fc73cf0950c6c11f6296393b56e8
doliveira.apolinario <doliveira.apolinario@gmail.com>
2017-04-06 00:32:34 -0300

    copied the all classes from other project

--- src/main/java/br/com/escola/rest/EscolaService.java
+++ src/main/java/br/com/escola/rest/EscolaService.java
@@ -1,9 +1,27 @@
 package br.com.escola.rest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
 public class EscolaService extends Application {
 
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
+ 
+	public EscolaService() {
+		singletons.add(new AlunoResource());
+	}
+ 
+	public Set<Class<?>> getClasses() {
+		return empty;
+	}
+ 
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+	
 }
