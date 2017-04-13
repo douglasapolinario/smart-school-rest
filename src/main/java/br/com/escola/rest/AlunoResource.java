22bcfc3f987f3bea3b2a1f64ebd05679813bf060
doliveira.apolinario <doliveira.apolinario@gmail.com>
2017-04-12 22:39:25 -0300

    teste rest get

--- src/main/java/br/com/escola/rest/AlunoResource.java
+++ src/main/java/br/com/escola/rest/AlunoResource.java
@@ -1,6 +1,7 @@
 package br.com.escola.rest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -109,20 +110,20 @@ public class AlunoResource {
 		aluno.setCelular("971634740");
 		aluno.setTelefone("47078194");
 		
-//		Iterable<Disciplina> disciplinas = disciplinaDao.findAll();
-//		Disciplina disciplina = disciplinas.iterator().hasNext() ? disciplinas.iterator().next() : new Disciplina();
-//		disciplina.setDescricao("Matematica");
-//
-//		Set<Disciplina> disciplinas2 = new HashSet<>();
-//		disciplinas2.add(disciplina);
-//		aluno.setDisciplinas(disciplinas2);
-//	
-//		alunoDao.createOrUpdate(aluno);
-//		
-//		Iterable<Pessoa> findAll = alunoDao.findAll();
-//		Pessoa pessoas = alunoDao.find(8l);
+		Iterable<Disciplina> disciplinas = disciplinaDao.findAll();
+		Disciplina disciplina = disciplinas.iterator().hasNext() ? disciplinas.iterator().next() : new Disciplina();
+		disciplina.setDescricao("Matematica");
+
+		Set<Disciplina> disciplinas2 = new HashSet<>();
+		disciplinas2.add(disciplina);
+		aluno.setDisciplinas(disciplinas2);
+	
+		alunoDao.createOrUpdate(aluno);
 		
-		return Arrays.asList(aluno);
+		Iterable<Pessoa> findAll = alunoDao.findAll();
+		List<Pessoa> pessoas = new ArrayList<>();
+		findAll.forEach(pessoas::add);
+		return pessoas;
 	}
 	
 	@GET
